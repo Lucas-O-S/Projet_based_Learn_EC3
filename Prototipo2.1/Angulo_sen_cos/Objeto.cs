34c9ec7e8906f0f92433cb7ad1110cdc76aa3282
Lucas-O-S <127321390+Lucas-O-S@users.noreply.github.com>
2023-10-17 10:59:42 -0300

    Versão com pequenas melhorias da 2.0
    Variáveis encapsuladas e devolução das coordenadas x e y final do gerenciador foram separadas

---
+++ Prototipo2.1/Angulo_sen_cos/Objeto.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetilTeste
+{
+    public class Objeto
+    {
+        //Classe pai de meteoro e projetil
+        private Double velocidadeInicial;
+        private Double PosicaoAtualX, PosicaoAtualY, PosicaoX0, PosicaoY0;
+
+        //Construtor base
+        public Objeto(double velocidadeInicial)
+        {
+            this.velocidadeInicial = velocidadeInicial;
+
+        }
+
+        public double VelocidadeInicial{ get{return velocidadeInicial;} set { velocidadeInicial = value; } }
+        public double posicaoAtualX { get { return PosicaoAtualX; } set { PosicaoAtualX = value; } }
+        public double posicaoAtualY { get { return PosicaoAtualY; } set { PosicaoAtualY = value; } }
+        public double posicaoX0 { get { return PosicaoX0; } set { PosicaoX0 = value; } }
+        public double posicaoY0 { get { return PosicaoY0; } set { PosicaoY0 = value; } }
+
+
+    }
+}
---
+++ Prototipo2.1/Angulo_sen_cos/Projetil.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetilTeste
+{
+    class Projetil : Objeto
+    {
+        private double VX,VY,cos,sen;
+        private int angulo;
+
+        public double vX { get { return VX; } set { VX = value; } }
+
+        public double vY { get { return VY; } set { VY = value; } }
+        public int AnguloLancamento { get { return angulo; } set { angulo = value; } }
+
+
+        //Construtor de projetil
+        public Projetil(double velocidadeInicial, int angulo, double presicao)
+            : base(velocidadeInicial)
+        {
+
+
+            this.VelocidadeInicial = velocidadeInicial;
+            this.angulo = angulo;
+            posicaoX0 = 0;
+            posicaoY0 = 0;
+            cos = Angulo.Cos(angulo, presicao);
+            sen = Angulo.Sen(angulo, presicao);
+            VX = FormulasFisica.VX0(velocidadeInicial, cos);
+            VY = FormulasFisica.VY0(velocidadeInicial, sen);
+
+
+
+
+        }
+
+        //Calcula o Movimento de X
+        public void MovX (double tempoAgora){
+            posicaoAtualX = FormulasFisica.MovX(posicaoX0, tempoAgora, VX);
+        }
+
+        //Calcula o Movimento de X
+        public void MovY(double tempoAgora)
+        {
+            posicaoAtualY = FormulasFisica.MovYSubindo(posicaoY0, tempoAgora, vY);
+        }
+
+
+        //Alterar valores 
+        public void MudarValores(double velocidadeInicial,int angulo, double presicao)
+        {
+            this.angulo = angulo;
+            this.VelocidadeInicial = velocidadeInicial;
+            cos = Angulo.Cos(angulo, presicao);
+            sen = Angulo.Sen(angulo, presicao);
+            VX = FormulasFisica.VX0(velocidadeInicial, cos);
+            VY = FormulasFisica.VY0(velocidadeInicial, sen);
+
+        }
+
+    }
+}
