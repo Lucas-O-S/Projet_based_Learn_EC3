19f232e206b8c84b76ad179688f9452efac02d19
Lucas-O-S <127321390+Lucas-O-S@users.noreply.github.com>
2023-11-18 10:39:44 -0300

    Update TelaConnect.cs
    Adição de comentarios

--- Projeto Final 1.0/Angulo_sen_cos/TelaConnect.cs
+++ Projeto Final 1.0/Angulo_sen_cos/TelaConnect.cs
@@ -8,8 +8,10 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+//Tela inicial de conexão com BD
 namespace ProjetilTeste
 {
+    
     public partial class TelaConnect : Form
     {
         
@@ -24,14 +26,19 @@ namespace ProjetilTeste
 
         }
 
+        //Botão que faz conexão com o banco de dados
         private void btnConnect_Click(object sender, EventArgs e)
         {
+            //Variavel que devolve a resposta se foi possivel conectar
             string Mensagem;
             try
             {
+                //Busca o nome do servidor, do usuario e a senha para conectar ao BD
                 Conexao.CriarConexao(boxServidor.Text, BoxNome.Text, boxSenha.Text);
+                //Testa a conexão
                 Conexao.conectar();
                 Mensagem = "Conexão foi Estabelecida";
+                //Cria um usuario para seguir para a proxima parte
                 Cadastro.CadastroUsuario(BoxNome.Text, "Email");
 
             }
@@ -40,6 +47,7 @@ namespace ProjetilTeste
                 Mensagem = "Conexão Falhou";
 
             }
+            //Mostra a mensagem
             StatusConnect.Visible = true;
             StatusConnect.Text = Mensagem;
 
@@ -47,7 +55,7 @@ namespace ProjetilTeste
 
         private void btnSeguir_Click(object sender, EventArgs e)
         {
-
+            //Segue para a tela inicial
             TelaInicial TI = new TelaInicial();
             TI.Show();
             this.Close();
